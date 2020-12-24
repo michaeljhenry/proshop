dac2fc67b2e627be126491fec824814e26534afb
Michael Henry <michaeljhenry@hotmail.ca>
2020-12-23 22:33:38 -0500

    checkout process part 2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,53 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc        Get order by ID
+// @route       GET /api/orders/:id
+// @access      Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  let orderId = req.params.id;
+  const order = await Order.findById(orderId).populate("user", "name email");
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc        Update order to paid
+// @route       PUT /api/orders/:id/pay
+// @access      Private
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  let orderId = req.params.id;
+  const order = await Order.findById(orderId);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.emaiL_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc        Get logged in user orders
+// @route       GET /api/orders/myorders
+// @access      Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
