1aafc94a1e3108c1c672bcf2cbd2df3646d5a333
Michael Henry <michaeljhenry@hotmail.ca>
2020-12-25 00:55:47 -0500

    custom page titles & meta

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -13,6 +13,7 @@ import {
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Rating from "../components/Rating";
+import Meta from "../components/Meta";
 import {
   listProductDetails,
   createProductReview,
@@ -72,6 +73,7 @@ const ProductScreen = ({ history, match }) => {
         <Message variant="danger">{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
